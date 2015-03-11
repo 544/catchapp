f85afb749a21eb36762f4cccb84990ba50845beb
masato <masato.tsukagoshi@O-02935-MAC.local>
2015-03-11 20:04:51 +0900

    add title scene empty

---
+++ Classes/TitleScene.cpp
@@ -0,0 +1,9 @@
+//
+//  TitleScene.cpp
+//  Catch
+//
+//  Created by Masato Tsukagoshi on 2015/03/11.
+//
+//
+
+#include "TitleScene.h"
---
+++ Classes/TitleScene.h
@@ -0,0 +1,14 @@
+//
+//  TitleScene.h
+//  Catch
+//
+//  Created by Masato Tsukagoshi on 2015/03/11.
+//
+//
+
+#ifndef __Catch__TitleScene__
+#define __Catch__TitleScene__
+
+#include <stdio.h>
+
+#endif /* defined(__Catch__TitleScene__) */
